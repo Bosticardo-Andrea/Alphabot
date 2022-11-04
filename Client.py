18baac57061a3e37ec314845847fd4638edb8f43
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2022-11-04 11:50:49 +0100

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,18 +1,36 @@
 #client TCP che permette di mandare comandi basilari all' alphabot
-import socket
+import socket,keyboard,time
+def comandiScritti(s):
+     while 1:
+        comando = input("inserisci comando: ")
+        if comando in ["f","b","l","r","s"]:
+            if comando == "s":
+                s.sendall("".join(comando + ",0").encode())
+            else:
+                tempo = int(input("inserisci un intero per i secondi e 0 per INFINITO: "))
+                if comando in ["f","b"]:
+                    speed = input("inserisci speed (ideale: 40): ")
+                    if int(speed) > 0 and int(speed) < 100:
+                        s.sendall("".join(comando + "," + str(tempo)+ "," + speed).encode())
+                    elif comando in ["r","l"]:
+                        s.sendall("".join(comando + "," + str(tempo)).encode())
+def comandiDaTastiera(s):
+    dizioComadi = {"space":"s","w":"f","s":"b","a":"l","d":"r",}
+    while 1:
+        s.sendall(dizioComadi[keyboard.read_key()])
+        time.sleep(0.1)
+
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("192.168.0.129",5000))
-    while True:
-        comando = input("inserisci comando: ")
-        if comando in ["f","b","l","r","s"]:
-            if comando in ["f","b"]:
-                speed = input("inserisci speed (ideale: 40): ")
-                if int(speed) > 0 and int(speed) < 100:
-                    s.sendall("".join(comando + "," + speed).encode())
-            else:
-                s.sendall(comando.encode())
+    risp = input("Vuoi scrivere i comandi oppure comandare il robot da tastiera [s/t]: ").lower()
+    while risp != "s" and risp != "t":
+        risp = input("Risposta sbagliata\n\nVuoi scrivere i comandi oppure comandare il robot da tastiera [s/t]: ").lower()
+    if risp == "s":
+        comandiScritti(s)
+    else:
+        comandiDaTastiera(s)
 
 if __name__ == "__main__":
     main()
--- Server.py
+++ Server.py
@@ -1,22 +1,38 @@
-import socket
+from multiprocessing.resource_sharer import stop
+import socket,time,os
 import AlphaBot 
-
 alpha = AlphaBot.AlphaBot()
 dizio = {"s": alpha.stop,"f":alpha.forward,"b":alpha.backward,"l":alpha.left,"r":alpha.right}
 def main():
+    es = os.popen("vcgencmd get_throttled")
+    ris = es.read()
+    print(ris)
+    if ris == "throttled=0x50005":print("SONO SCARICO")
+    else:print("TUTTO OK")
+
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind(("0.0.0.0",5000))
     print("Sto ascoltando...")
     s.listen()
     connection,address = s.accept()
+    print(f"Si é connesso: {address[0]}")
     while 1:
         dato = connection.recv(4096).decode().lower()
         print(dato)
-        if "," in dato:
-            dati = dato.split(",")
-            dizio[dati[0]](int(dati[1]))
+        dati = dato.split(",")
+        if dati[0] == "r" or dati[0] == "l":
+            dizio[dati[0]]()
+            time.sleep(int(dati[1]))
+            alpha.stop()
+        elif int(dati[1]) == 0:
+            if int(dati[2]) == 0:
+                dizio[dati[0]]()
+            else:
+                dizio[dati[0]](int(dati[2]))
         else:
-            dizio[dati[0]]
+            dizio[dati[0]](int(dati[2]))
+            time.sleep(int(dati[1]))
+            alpha.stop()
         
 if __name__=="__main__":
     main()
